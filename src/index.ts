37c905a0aec283b86fa10f4237129683016acf38
Nicholas Carrigan <nhcarrigan@gmail.com>
2021-03-07 10:40:42 -0800

    feat: update text output
    Updates and improves the text output generated by the package.

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,40 @@
 import chalk from "chalk";
 
-const nhcarrigan = () => {
-  console.info(
-    chalk.magenta(
-      "Hello! My name is Nicholas Carrigan, and this is my npm package!\nSorry to say, this package does not actually DO anything. It is just here for fun. However...\nI have written plenty of projects that ARE functional. If you would like to check out my work, please visit my website at: \n"
-    ) +
-      chalk.black.bgMagenta("https://www.nhcarrigan.com") +
-      chalk.magenta("\nOr view my GitHub:\n") +
-      chalk.black.bgMagenta("https://github.com/nhcarrigan") +
-      chalk.magenta("\nThanks for checking this package out! Have a great day!")
-  );
+export const text: { [key: string]: string } = {
+  about:
+    "Hello! My name is Nicholas Carrigan. I am a TypeScript developer with an obsession for chat bots and CLI tools.",
+  packageIntro:
+    "This is my npm package! Sorry to say, this package does not actually DO anything. It is just here for fun. However...",
+  websiteIntro:
+    "I have written plenty of projects that ARE functional. If you would like to check out my work, please visit my website at:",
+  websiteUrl: "https://www.nhcarrigan.com",
+  githubIntro:
+    "You can also view all of my projects, including source code and links to live instances, on my GitHub:",
+  githubUrl: "https://github.com/nhcarrigan",
+  beccaIntro:
+    "I also have quite a few projects that are branded under Becca Lyria, my old Dungeons and Dragons character.",
+  beccaWebsiteIntro: "You can read more about Becca at her website:",
+  beccaWebsiteUrl: "https://www.beccalyria.com",
+  beccaDiscordIntro: "Also, check out the Becca Lyria Discord bot!",
+  beccaDiscordInviteUrl:
+    "Invite the bot to your server: http://invite.beccalyria.com",
+  beccaDiscordDocsUrl:
+    "View the bot documentation: https://www.beccalyria.com/discord-documentation",
+};
+
+export const nhcarrigan = (): void => {
+  console.info(chalk.cyan(text.about));
+  console.info(chalk.cyan(text.packageIntro));
+  console.info(chalk.green(text.websiteIntro));
+  console.info(chalk.green(text.websiteUrl));
+  console.info(chalk.yellow(text.githubIntro));
+  console.info(chalk.yellow(text.githubUrl));
+  console.info(chalk.magenta(text.beccaIntro));
+  console.info(chalk.magenta(text.beccaWebsiteIntro));
+  console.info(chalk.magenta(text.beccaWebsiteUrl));
+  console.info(chalk.magenta(text.beccaDiscordIntro));
+  console.info(chalk.magenta(text.beccaDiscordInviteUrl));
+  console.info(chalk.magenta(text.beccaDiscordDocsUrl));
 };
 
 nhcarrigan();
